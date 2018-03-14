d17bca3166fbf9cf15e0eea2cc5912d007469f3a
suratt <you@example.com>
2018-03-14 15:49:09 +0800

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+    apiKey: "AIzaSyCccxtuzCknd48FyXrSV8Pt9UDCoAiuDTo",
+    authDomain: "angulars-9eacd.firebaseapp.com",
+    databaseURL: "https://angulars-9eacd.firebaseio.com",
+    projectId: "angulars-9eacd",
+    storageBucket: "angulars-9eacd.appspot.com",
+    messagingSenderId: "541170937490"
     }
 };
